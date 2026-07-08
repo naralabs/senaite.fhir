7634fb9947c43009078d340fa08b8a2e415e7631
nnpvaan <phuongvan0609@gmail.com>
2026-07-08 11:38:34 +1000

    Enforce canonical field order in ResultsBundleResource JSON output

--- src/senaite/fhir/resource/resultsbundle.py
+++ src/senaite/fhir/resource/resultsbundle.py
@@ -1,18 +1,40 @@
 # -*- coding: utf-8 -*-
 
+from collections import OrderedDict
+
 from senaite.fhir.interfaces import IResultsBundleResource
 from senaite.fhir.resource import FHIRResource
 from zope.interface import implementer
 
+FIELD_ORDER = (
+    "resourceType",
+    "id",
+    "meta",
+    "type",
+    "timestamp",
+    "total",
+    "entry",
+)
+
 
 @implementer(IResultsBundleResource)
-class ResultsBundleResource(FHIRResource):
+class ResultsBundleResource(FHIRResource, OrderedDict):
     """A searchset Bundle returned by the DiagnosticReport polling endpoint.
     Contains SenaiteDiagnosticReport match entries and SenaiteObservation
     include entries. The presentedForm.data (PDF) is excluded when the request
     uses _summary=true.
     https://fhir.senaite.org/StructureDefinition-SenaiteResultsBundle.html
     """
+
+    def _initialize(self):
+        data = dict(self)
+        self.clear()
+        for k in FIELD_ORDER:
+            if k in data:
+                self[k] = data[k]
+        for k, v in data.iteritems():
+            if k not in FIELD_ORDER:
+                self[k] = v
 
     @property
     def type(self):
