fcaa6e2ef5d123c1a750c4a962e31ad1351ce9ae
Jordi Puiggené <jp@naralabs.com>
2026-07-19 17:50:23 +0200

    Undo sorting of attributes for ResultsBundleResource

--- src/senaite/fhir/resource/resultsbundle.py
+++ src/senaite/fhir/resource/resultsbundle.py
@@ -1,40 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
-
 from senaite.fhir.interfaces import IResultsBundleResource
 from senaite.fhir.resource import FHIRResource
 from zope.interface import implementer
 
-FIELD_ORDER = (
-    "resourceType",
-    "id",
-    "meta",
-    "type",
-    "timestamp",
-    "total",
-    "entry",
-)
-
 
 @implementer(IResultsBundleResource)
-class ResultsBundleResource(FHIRResource, OrderedDict):
+class ResultsBundleResource(FHIRResource):
     """A searchset Bundle returned by the DiagnosticReport polling endpoint.
     Contains SenaiteDiagnosticReport match entries and SenaiteObservation
     include entries. The presentedForm.data (PDF) is excluded when the request
     uses _summary=true.
     https://fhir.senaite.org/StructureDefinition-SenaiteResultsBundle.html
     """
-
-    def _initialize(self):
-        data = dict(self)
-        self.clear()
-        for k in FIELD_ORDER:
-            if k in data:
-                self[k] = data[k]
-        for k, v in data.iteritems():
-            if k not in FIELD_ORDER:
-                self[k] = v
 
     @property
     def type(self):
